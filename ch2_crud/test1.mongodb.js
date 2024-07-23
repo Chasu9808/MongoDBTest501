40cf74c3ef3e0a4eb8da9a680698d337ae66391e
이상용 <lsy3709@naver.com>
2024-07-23 12:40:03 +0900

    4교시, crud 간단 버전

--- ch2_crud/test1.mongodb.js
+++ ch2_crud/test1.mongodb.js
@@ -1,15 +1,18 @@
 //capped collection, 오래된 문서 순서로 삭제 확인. 
-use("testDB");
+// use("testDB");
 // 특정 크기의 컬렉션을 생성, 용량이 초과하면, 오래된 데이터 부터 삭제함. 확인.
+// 한줄 실행, ctrl + alt + s
+// 전체 실행, ctrl + alt + r
+
 db.createCollection("cappedC", { capped: true, size: 10000 });
 db.cappedC.insertOne({ x: 1 });
 db.cappedC.find();
 
 //연습2
-use("testDB");
+// use("testDB");
 // 특정 크기의 컬렉션을 생성, 용량이 초과하면, 오래된 데이터 부터 삭제함. 확인.
-db.createCollection("cappedC2", { capped: true, size: 10000 });
-use("testDB");
+db.createCollection("cappedC2");
+// use("testDB");
 db.cappedC2.insertOne({ x: 1 });
 db.cappedC2.find();
 
@@ -17,28 +20,30 @@ db.cappedC2.find();
 // use("testDB");
 // db.cappedC.stats();
 
-use("testDB");
-for (i = 0; i < 1000; i++) {
-  db.cappedC.insertOne({ x: i });
-}
-
-//연습2
-use("testDB");
+// use("testDB");
 for (i = 0; i < 1000; i++) {
   db.cappedC2.insertOne({ x: i });
 }
 
-use("testDB");
+//연습2
+// use("testDB");
+for (i = 0; i < 1000; i++) {
+  db.cappedC2.insertOne({ x: i });
+}
+
+// use("testDB");
 // 앞에, 1 부터 646 까지 다 삭제됨.
 // 오래된 데이텀.
 db.cappedC.find();
+db.cappedC.storageSize();
+db.cappedC.stats();
 
-use("testDB");
+// use("testDB");
 // 앞에, 1 부터 646 까지 다 삭제됨.
 // 오래된 데이터.
 db.cappedC2.find();
 
 // 서버 상태 조회.
-use("testDB");
+// use("testDB");
 // db.getCollectionInfos();
 db.serverStatus();
